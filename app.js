f577bb6092aae77cfb312ab266326715cb88ce85
zadikhan <zadkhan51@gmail.com>
2023-01-21 20:56:34 +0530

    other changes

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.get('/registration', (req, res) => {
 })
 if(process.env.NODE_ENV =="production"){
     app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(_dirname,'client','build','index.html'));
+    })
 }
 
 app.listen(PORT, () => {
